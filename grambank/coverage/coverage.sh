01909d959598170c73162fb61304d2d847c143c4
xrotwang <xrotwang@googlemail.com>
2016-05-20 13:14:42 +0200

    recreated coverage data

--- grambank/coverage/coverage.sh
+++ grambank/coverage/coverage.sh
@@ -2,4 +2,4 @@
 psql -d glottolog3 -A --field-separator='+++' -t -f query_languages.sql -o languages.csv
 psql -d glottolog3 -A --field-separator='+++' -t -f query_macroareas.sql -o macroareas.csv
 python coverage.py
-
+cp *.json ../static
