ce9d99d2c6c75945d12e5cd90bf29b898e2ed66e
chrzyki <christoph@functor-argument.net>
2018-02-12 10:32:26 +0100

    Update scaffolding etc.

--- setup.py
+++ setup.py
@@ -20,15 +20,20 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        'clld~=4.0',
         'clldmpg~=3.1.0',
         'clld-glottologfamily-plugin>=2.0.0',
         'pyglottolog>=0.3.1',
+        'sqlalchemy',
+        'waitress',
     ],
     extras_require={
-        'dev': ['flake8', 'waitress'],
-        'test': [
+        'dev': [
+            'flake8',
             'psycopg2',
             'tox',
+        ],
+        'test': [
             'mock',
             'pytest>=3.1',
             'pytest-clld',
